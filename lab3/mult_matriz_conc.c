729b51507b8dad3a4fa41579bdb161717a2f9cf5
Flávio M <flavio.munis42@gmail.com>
2024-09-01 13:25:43 -0300

    concurrent solution implemented

--- lab3/mult_matriz_conc.c
+++ lab3/mult_matriz_conc.c
@@ -1,10 +1,10 @@
 /*-----------------------------------------------------------------*/
 /**
 
-  @file    mult_matriz_seq.c
+  @file    mult_matriz_conc.c
   @author  Flávio M.
-  @brief   Multiplica Duas Matrizes (M X N) e (N X M) e escreve a
-           matriz resultado num arquivo binário.
+  @brief   Multiplica Duas Matrizes (M X N) e (N X M) de forma concorrente
+           e escreve a matriz resultado num arquivo binário.
   @Materia Prog Concorrente (ICP361)
 
  */
@@ -18,8 +18,23 @@
 #include <stdbool.h>
 #include <errno.h>
 #include <time.h>
+#include <pthread.h>
 #include "timer.h"
 #include "error-handler.h"
+
+
+/*-----------------------------------------------------------------
+                              Structs
+  -----------------------------------------------------------------*/
+typedef struct multInfo {
+    unsigned int startRow;
+    unsigned int endRow;
+	unsigned int n;
+	unsigned int m;
+	float** m1;
+	float** m2;
+	float** result;
+} MultInfo;
 
 
 /*-----------------------------------------------------------------
@@ -96,9 +111,10 @@ void readFromFile(void*, size_t, size_t, FILE*);
    @brief Check if a String of Arguments is Valid.
    @param int             Total Arguments in String (argc).
    @param char*           String of Arguments (argv).
+   @param unsigned short* Pointer to Threads.
 */
 /*-----------------------------------------------------------------*/
-void checkArgs(int, char*[]);
+void checkArgs(int, char*[], unsigned short*);
 
 
 /*-----------------------------------------------------------------*/
@@ -111,15 +127,73 @@ void checkArgs(int, char*[]);
    @return float**      Result Matrix (M x M).
 */
 /*-----------------------------------------------------------------*/
-float** multMatrix(float**,
-				   float**,
-				   unsigned int,
-				   unsigned int);
+void* multMatrix(void*);
 
 
 /*-----------------------------------------------------------------
                       Functions Implementation
   -----------------------------------------------------------------*/
+
+MultInfo* initMultInfo() {
+
+	MultInfo* newInfo = (MultInfo*) malloc(sizeof(MultInfo));
+	checkNullPointer((void*) newInfo);
+
+	newInfo -> m1 = NULL;
+	newInfo -> m2 = NULL;
+	newInfo -> result = NULL;
+	
+	return newInfo;
+}
+
+void printInfo(MultInfo* info) {
+
+	unsigned int start = info -> startRow;
+	unsigned int end = info -> endRow;
+	unsigned int n = info -> n;
+	unsigned int m = info -> m;
+	float** m1 = info -> m1;
+	float** m2 = info -> m2;
+	unsigned int inter = end - start;
+	
+	puts("\n=== Info ===");
+	printf("M1 Interval Size: %u\n", inter);
+    puts("Result Matrix: ");
+
+	
+    for(unsigned int i = start; i < end; i++) {
+
+		float* currM1 = m1[i];
+
+		if(i == start)
+			printf("[");
+		
+		for(unsigned int j = 0; j < m; j++) {
+			if(!j)
+				printf("[");
+
+			for(unsigned int k = 0; k < n; k++) {
+				if(k == n - 1)
+					printf("%f * %f", currM1[k], m2[k][j]);
+				else
+					printf("%f * %f + ", currM1[k], m2[k][j]);
+			}
+
+			if(j == m - 1)
+				printf("]");
+			else
+				printf(", ");
+
+			if(i != end - 1)
+				printf("\n");
+		}
+
+		if(i == end - 1)
+			printf("]\n");
+	}
+
+	puts("");
+}
 
 void printMatrix(float** matriz,
 				 unsigned int rows,
@@ -179,11 +253,22 @@ void writeToFile(void* info, size_t size, size_t total, FILE* out) {
 }
 
 void checkArgs(int argc,
-			   char* argv[]) {
+			   char* argv[],
+			   unsigned short* threads) {
 
-	if (argc != 3) {
-		invalidArgumentError("Usage: \n  ./[program] [input_file] [output_file]");
+	unsigned int th;
+	
+	if (argc != 4) {
+		invalidArgumentError("Usage: \n  ./[program] [input_file] [output_file] [threads]");
 	}
+
+	th = atoi(argv[3]);
+	
+	if(th < 1 || th > 65536) {
+		invalidArgumentError("Invalid Number of Threads! 1 < Threads < 65536");
+	}
+
+	*threads = th;
 }
 
 void getInputData(char* inputPath,
@@ -264,78 +349,136 @@ void writeOutput(char* outputPath,  unsigned int m, float** result) {
 	fclose(output);
 }
 
-float** multMatrix(float** matriz1,
-				   float** matriz2,
-				   unsigned int m,
-				   unsigned int n) {
+void* multMatrix(void* arg) {
+	MultInfo* info = (MultInfo*) arg;
+	unsigned int start = info -> startRow;
+	unsigned int end = info -> endRow;
+	unsigned int n = info -> n;
+	unsigned int m = info -> m;
+	float** m1 = info -> m1;
+	float** m2 = info -> m2;
+	unsigned int inter = end - start;
 	float** result;
 	float soma = 0.0;
 	
-	result = (float**) malloc(sizeof(float*) * m);
+	result = (float**) malloc(sizeof(float*) * inter);
 	checkNullPointer((void*) result);
 
-	for(unsigned int i = 0; i < m; i++) {
+	for(unsigned int i = 0; i < inter; i++) {
 		result[i] = (float*) malloc(sizeof(float) * m);
 		checkNullPointer((void*) result);
 	}
+	
+    for(unsigned int i = start; i < end; i++) {
 
-	for(unsigned int i = 0; i < m; i++) {
+		float* currM1 = m1[i];
+		
 		for(unsigned int j = 0; j < m; j++) {
+
 			soma = 0.0;
 			
-			for(unsigned int k = 0; k < n; k++) {
-			    soma += matriz1[i][k] * matriz2[k][j];
-			}
+			for(unsigned int k = 0; k < n; k++)
+				soma += currM1[k] * m2[k][j];
 
-			result[i][j] = soma;
+			result[i - start][j] = soma;
 		}
 	}
+
+	info -> result = result;
 	
-	return result;
+	return (void*) info;
 }
 
 int main(int argc, char* argv[]) {
 
 	unsigned int m, n;
+	unsigned short threads;
     float** matriz1, **matriz2;
 	float** result;
 	MyTimer* timerIORead, *timerIOWrite, *timerMult;
 	
-    checkArgs(argc, argv);
+    checkArgs(argc, argv, &threads);
 
 	INIT_TIMER(timerIORead);
     getInputData(argv[1], &m, &n, &matriz1, &matriz2);
     END_TIMER(timerIORead);
 
-	INIT_TIMER(timerMult);
-	result = multMatrix(matriz1, matriz2, m, n);
-	END_TIMER(timerMult);
+	if(threads > m) {
+		invalidArgumentError("More Threads Than Rows, Insert A Valid Number of Threads!");
+	}
+		
+	pthread_t th[threads];
+	unsigned int rowsPerPart = m / threads;
+	unsigned int start, end;
 
+	//printMatrix(matriz1, m, n);
+	//printMatrix(matriz2, n, m);
+
+	INIT_TIMER(timerMult);
+	
+	for(unsigned short i = 0; i < threads; i++) {
+
+		MultInfo* info = initMultInfo();
+
+	    // Calc Current Chunk To Send To Thread
+		start = rowsPerPart * i;
+
+		if (i == threads - 1)
+			end = m;
+		else
+			end = rowsPerPart * (i + 1);
+
+		info -> startRow = start;
+		info -> endRow = end;
+		info -> n = n;
+		info -> m = m;
+		info -> m1 = matriz1;
+		info -> m2 = matriz2;
+
+		//printInfo(info);
+		
+		if(pthread_create(th + i, NULL, &multMatrix, (void*) info) != 0) {
+			unexpectedError("Error Creating Threads!");
+		}
+	}
+
+	result = (float**) malloc(sizeof(float*) * m);
+	checkNullPointer((void*) result);
+	
+	for(unsigned short i = 0; i < threads; i++) {
+
+	    MultInfo* retInfo;
+	    float** tempResult;
+		unsigned int startInfo, endInfo;
+		
+		if(pthread_join(th[i], (void**) &retInfo) != 0) {
+			unexpectedError("Error Joining Threads!");
+		}
+
+		startInfo = retInfo -> startRow;
+		endInfo = retInfo -> endRow;
+		tempResult = retInfo -> result;
+		
+		for(unsigned int j = startInfo; j < endInfo; j++) 
+			result[j] = tempResult[j - startInfo];
+		
+
+		free(tempResult);
+		free(retInfo);
+	}
+
+	END_TIMER(timerMult);
+	//printMatrix(result, m, m);
+	
 	INIT_TIMER(timerIOWrite);
 	writeOutput(argv[2], m, result);
 	END_TIMER(timerIOWrite);
 	
-	//printMatrix(matriz1, m, n);
-	//printMatrix(matriz2, n, m);
-	//printMatrix(result, m, m);
-
-	CALC_FINAL_TIME(timerIORead);
-	CALC_FINAL_TIME(timerIOWrite);
-	CALC_FINAL_TIME(timerMult);
-
-	puts("=== Time Elapsed ===\n");
-	printf("Read IO: %.5fs\n", timerIORead -> totalTime);
-	printf("Mult.: %.5fs\n", timerMult -> totalTime);
-	printf("Write IO: %.5fs\n", timerIOWrite -> totalTime);
-	printf("Total Time: %.5fs\n", timerIOWrite -> totalTime +
-		   timerIORead -> totalTime +
-		   timerMult -> totalTime);
-	
-    // Check if its a square matrix
+	// Check if its a square matrix
 	if(m != n) {
 		for(unsigned int i = 0; i < m; i++){
 			free(matriz1[i]);
-			free(result);
+			free(result[i]);
 		}
 		
 		for(unsigned int i = 0; i < n; i++)
@@ -352,6 +495,20 @@ int main(int argc, char* argv[]) {
 	free(matriz1);
 	free(matriz2);
 	free(result);
+
+	CALC_FINAL_TIME(timerIORead);
+	CALC_FINAL_TIME(timerIOWrite);
+	CALC_FINAL_TIME(timerMult);
+	
+	puts("=== Time Elapsed ===\n");
+	printf("Read IO: %.5fs\n", timerIORead -> totalTime);
+	printf("Mult.: %.5fs\n", timerMult -> totalTime);
+	printf("Write IO: %.5fs\n", timerIOWrite -> totalTime);
+	printf("Total Time: %.5fs\n", timerIOWrite -> totalTime +
+		   timerIORead -> totalTime +
+		   timerMult -> totalTime);
+	
+    
 	free(timerIORead);
 	free(timerMult);
 	free(timerIOWrite);
