6bad6865cef96f9479fdf52cd6c3a423b2c22b9f
Flávio M <flavio.munis42@gmail.com>
2024-09-30 09:09:08 -0300

    lista 1 bbp algo sequencial

---
+++ lista1/include/error-handler.h
@@ -0,0 +1,99 @@
+/*-----------------------------------------------------------------*/
+/**
+
+  @file   error-handler.h
+  @author Flávio M.
+
+ */
+/*-----------------------------------------------------------------*/
+#ifndef ERROR_HANDLER_HEADER_FILE
+#define ERROR_HANDLER_HEADER_FILE
+
+
+/*-----------------------------------------------------------------
+                              Includes
+  -----------------------------------------------------------------*/
+#include <errno.h>
+
+
+/*-----------------------------------------------------------------
+                          Macros Definitions
+  -----------------------------------------------------------------*/
+
+/*-----------------------------------------------------------------*/
+/**
+   @brief Halts Program and Prints An Custom Error Message And
+          System Menssage.
+   @param char* Any String That Will Be Printed as Custum Error
+ */
+/*-----------------------------------------------------------------*/
+#define FATAL_SYS_ERROR(err) \
+    perror(err);	\
+	exit(EXIT_FAILURE);
+
+
+/*-----------------------------------------------------------------*/
+/**
+   @brief Halts Program and Prints An Custom Error Message
+   @param char* Any String That Will Be Printed as Custum Error
+ */
+/*-----------------------------------------------------------------*/
+#define FATAL_USR_ERROR(err) \
+	fprintf(stderr, "\n%s\n", err);	\
+	exit(EXIT_FAILURE);
+
+
+/*-----------------------------------------------------------------*/
+/**
+   @brief Checks if a Given Pointer is NULL
+   @param p Is Any Pointer;
+
+   If Pointer is NULL, halts Program and prints an error message to user.
+ */
+/*-----------------------------------------------------------------*/
+#define checkNullPointer(p) if(!p) { FATAL_SYS_ERROR("Pointer Couldn't Be Allocated!"); } 
+
+
+/*-----------------------------------------------------------------*/
+/**
+   @brief Checks if a Given File Pointer is NULL
+   @param p Is A File Pointer;
+
+   If Pointer is NULL, halts Program and prints an error message to user.
+ */
+/*-----------------------------------------------------------------*/
+#define checkNullFilePointer(p) if(!p) { FATAL_SYS_ERROR("Invalid File Pointer!"); } 
+
+
+/*-----------------------------------------------------------------*/
+/**
+   @brief Throws an Error Message to User and Halts Program
+   @param char* Custom Error Message
+ */
+/*-----------------------------------------------------------------*/
+#define invalidArgumentError(err) { FATAL_USR_ERROR(err); }
+
+
+/*-----------------------------------------------------------------*/
+/**
+   @brief Throws an Error Message to User
+   @param char* Custom Error Message
+ */
+/*-----------------------------------------------------------------*/
+#define invalidArgumentException(exp) fprintf(stderr, "Invalid Argument Exception!\n%s\n", exp); 
+
+
+#define invalidProgramCall(progName, args) \
+	fprintf(stderr, "Usage: \n ./%s %s\n", progName, args); \
+    exit(EXIT_FAILURE);
+
+
+/*-----------------------------------------------------------------*/
+/**
+   @brief Unexpected Error in Code
+   @param char* Custom Error Message
+ */
+/*-----------------------------------------------------------------*/
+#define unexpectedError(err) { FATAL_SYS_ERROR(err); }
+
+#endif
---
+++ lista1/include/timer.h
@@ -0,0 +1,78 @@
+/*-----------------------------------------------------------------*/
+/**
+
+  @file   timer.h
+  @author Flávio M.
+
+ */
+/*-----------------------------------------------------------------*/
+
+#ifndef MY_TIMER_HEADER_FILE
+#define MY_TIMER_HEADER_FILE
+
+/*-----------------------------------------------------------------
+                              Includes
+  -----------------------------------------------------------------*/
+#include <time.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include "error-handler.h"
+
+
+/*-----------------------------------------------------------------
+                              Structs
+  -----------------------------------------------------------------*/
+typedef struct mytimer {
+    struct timespec start;
+    struct timespec end;
+	double totalTime;
+}MyTimer;
+
+
+/*-----------------------------------------------------------------
+                          Macros Definitions
+  -----------------------------------------------------------------*/
+
+/*-----------------------------------------------------------------*/
+/**
+   @brief Allocate Memory for MyTimer Struct
+   @param MyTimer* Pointer to MyTimer Struct.
+ */
+/*-----------------------------------------------------------------*/
+#define MALLOC_TIMER(timer)						\
+	timer = (MyTimer*) malloc(sizeof(MyTimer)); \
+	checkNullPointer((void*) timer);
+
+
+/*-----------------------------------------------------------------*/
+/**
+   @brief Malloc and Adds Init To Timer Struct
+   @param MyTimer* Pointer to MyTimer Struct.
+ */
+/*-----------------------------------------------------------------*/
+#define INIT_TIMER(timer) {  \
+		MALLOC_TIMER(timer); \
+	    clock_gettime(CLOCK_MONOTONIC_RAW, &(timer -> start));	\
+}
+
+
+/*-----------------------------------------------------------------*/
+/**
+   @brief Calculate Total Time Elapsed in Seconds.
+   @param MyTimer* Pointer to MyTimer Struct.
+ */
+/*-----------------------------------------------------------------*/
+#define CALC_FINAL_TIME(timer) \
+	timer -> totalTime = ((timer -> end).tv_nsec - (timer -> start).tv_nsec) / 1000000000.0 + ((timer -> end).tv_sec - (timer -> start).tv_sec);
+
+
+/*-----------------------------------------------------------------*/
+/**
+   @brief Add Stop To Timer Struct.
+   @param MyTimer* Pointer to MyTimer Struct.
+ */
+/*-----------------------------------------------------------------*/
+#define END_TIMER(timer) clock_gettime(CLOCK_MONOTONIC_RAW, &(timer -> end));
+
+
+#endif
