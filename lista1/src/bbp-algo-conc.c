7c09b5f1ac41e39a7f155e6235d3d9ca729bced4
Flávio M <flavio.munis42@gmail.com>
2024-10-02 21:12:09 -0300

    bbp algo with thread pool (terrible overhead)

--- lista1/src/bbp-algo-conc.c
+++ lista1/src/bbp-algo-conc.c
@@ -32,9 +32,9 @@
                               Structs
   -----------------------------------------------------------------*/
 typedef struct node {
-	void* (*func)(void*);
-	void* arg;
-	void* retValue;
+	long double (*func)(int, long long, long double);
+    int arg1;
+	long long arg2;
 	struct node* next;
 } Node;
 
@@ -48,10 +48,14 @@ typedef struct queue {
 /*-----------------------------------------------------------------
                           Global Variables -----------------------------------------------------------------*/
 pthread_mutex_t qsMutex, stopMutex, activeMutex;
+pthread_mutex_t s1Mutex, s2Mutex, s3Mutex, s4Mutex;
 pthread_cond_t qsCond, stopCond;
 bool stop = false;
 short int activeThreads;
+long long d;
 Queue* work;
+
+long double s1 = 0.0L, s2 = 0.0L, s3 = 0.0L, s4 = 0.0L;
 
 
 /*-----------------------------------------------------------------
@@ -65,7 +69,7 @@ Queue* work;
    @param char*           String of Arguments (argv).
 */
 /*-----------------------------------------------------------------*/
-void checkArgs(int, char*[], long long*);
+void checkArgs(int, char*[]);
 
 
 /*-----------------------------------------------------------------*/
@@ -75,7 +79,7 @@ void checkArgs(int, char*[], long long*);
    @return long double Fractional Part Containing The Result.
 */
 /*-----------------------------------------------------------------*/
-long double bbpAlgo(long long);
+long double bbpAlgo();
 
 
 /*-----------------------------------------------------------------*/
@@ -111,12 +115,13 @@ Queue* initQueue();
 /*-----------------------------------------------------------------*/
 /**
    @brief  Init a New Queue Node.
-   @param  void* (*f)(void*) Pointer to Node Function.
-   @param  void*             Argument to Function.
-   @return Node*             Pointer to New Node.
+   @param  int   Argument.
+   @return Node* Pointer to New Node.
 */
 /*-----------------------------------------------------------------*/
-Node* initNode(void* (*func)(void*), void*);
+Node* initNode(long double (*func)(int, long long, long double), 
+			   int, 
+			   long long);
 
 /*-----------------------------------------------------------------*/
 /**
@@ -130,12 +135,10 @@ void freeNode(Node*);
 /*-----------------------------------------------------------------*/
 /** (MUDAR)
    @brief Add a New Node to Queue
-   @param Queue*           Pointer to Queue.
-   @param void* (*f)(void*) Pointer to Node Function.
-   @param void*             Argument to Function.
+   @param Node*  Node to Be Added.
 */
 /*-----------------------------------------------------------------*/
-void enqueue(Queue*, Node*);
+void enqueue(Node*);
 
 
 /*-----------------------------------------------------------------*/
@@ -144,7 +147,7 @@ void enqueue(Queue*, Node*);
    @return Node* Pointer to Dequeued Node.
 */
 /*-----------------------------------------------------------------*/
-Node* dequeue(Queue*);
+Node* dequeue();
 
 
 /*-----------------------------------------------------------------*/
@@ -153,7 +156,7 @@ Node* dequeue(Queue*);
    @param Queue* Pointer to Queue.
 */
 /*-----------------------------------------------------------------*/
-void printQueue(Queue*);
+void printQueue();
 
 
 /*-----------------------------------------------------------------*/
@@ -184,14 +187,16 @@ Queue* initQueue() {
 	return newQ;
 }
 
-Node* initNode(void* (*func)(void*), void* arg) {
+Node* initNode(long double (*func)(int, long long, long double), 
+			   int arg1, 
+			   long long arg2) {
 	
 	Node* newNode = malloc(sizeof(Node));
 	checkNullPointer((void*) newNode);
-	
+    
 	newNode -> func = func;
-	newNode -> arg = arg;
-	newNode -> retValue = NULL;
+	newNode -> arg1 = arg1;
+	newNode -> arg2 = arg2;
 	newNode -> next = NULL;
 
 	return newNode;
@@ -199,76 +204,62 @@ Node* initNode(void* (*func)(void*), void* arg) {
 
 void freeNode(Node* node) {
 
-	if (node) {		
-		if (node -> arg)
-			free(node -> arg);
-
-		if (node -> retValue)
-			free(node -> retValue);
-
+	if (node)
 		free(node);
-	}
 }
 
-void enqueue(Queue* q, Node* newNode) {
-	
-	// If queue was not initialized yet
-	if (!q)
-		return;
+void enqueue(Node* newNode) {
 
 	pthread_mutex_lock(&qsMutex);
 	// If Queue is empty
-	if (!q -> rear) {
-	    q -> front = newNode;
-		q -> rear = newNode;
+	if (!work -> rear) {
+	    work -> front = newNode;
+		work -> rear = newNode;
 		
 	// If Queue already has elements
 	} else {
-		q -> rear -> next = newNode;
-		q -> rear = newNode;
+		work -> rear -> next = newNode;
+		work -> rear = newNode;
 	}
 
-    q -> totalNodes++;
+    work -> totalNodes++;
 	pthread_cond_signal(&qsCond);
 	pthread_mutex_unlock(&qsMutex);
 }
 
-Node* dequeue(Queue* q) {
+Node* dequeue() {
 
 	Node* temp = NULL;
 	
-	if (!q || !q -> totalNodes)
+	if (!work || !work -> totalNodes)
 		return NULL;
 
-	temp = q -> front;
-	q -> front = q -> front -> next;
-	q -> totalNodes--;
+	temp = work -> front;
+    work -> front = work -> front -> next;
+	work -> totalNodes--;
 
-	if (!q -> front)
-		q -> rear = NULL;
+	if (!work -> front)
+		work -> rear = NULL;
 
 	return temp;
 }
 
 
-void printQueue(Queue* q) {
+void printQueue() {
 
 	unsigned int i = 0;
 	Node* aux;
 
-	if (!q)
+	if (!work)
 		return;
 
-	aux = q -> front;
+	aux = work -> front;
 
 	while (aux) {
 		printf("=== Node %d ===\n", i++);
-		
-		if (aux -> arg)
-			printf("Arg = %d\n", *((int*)aux -> arg));
-		
-		if (aux -> retValue)
-			printf("RetValue = %Lf\n", *((long double*)aux -> retValue));
+	    
+		printf("Arg 1 = %d\n", aux -> arg1);
+	    printf("Arg 2 = %lld\n", aux -> arg2);
 
 		puts("");
 		aux = aux -> next;
@@ -277,52 +268,72 @@ void printQueue(Queue* q) {
 
 
 void checkArgs(int argc, 
-			   char* argv[],
-			   long long* d) {
-
-	long long digit;
+			   char* argv[]) {
 
 	if (argc != 3) {
 		invalidProgramCall(argv[0], "[inicio] [threads]");
 	}
 
-    digit = strtoll(argv[1], NULL, 10);
+    d = strtoll(argv[1], NULL, 10);
     activeThreads = strtoll(argv[2], NULL, 10);
 
-	if (digit < 0) {
+	if (d < 0) {
 		invalidArgumentError("Argumento Inválido!\nInicio >= 0");
 	}
 
 	if (activeThreads < 1 || activeThreads > 65536) {
 		invalidArgumentError("Invalid Numvber of Threads!\n1 < Threads < 65536");
 	}
-
-	*d = digit;
-}
-
-
-void* sqrt2(void* arg) {
-	int num = *((int*) arg);
-	int* result = (int*) malloc(sizeof(int));
-
-	*result = sqrt(num);
-	return (void*) result;
-}
-
-void retFunction(Node* n) {
-	int arg, retValue;
-
-	arg = *((int*) n -> arg);
-    retValue = *((int*) n -> retValue);
-
-	printf("%d * 2 = %d\n", arg, retValue);
 }
 
 void executeWork(Node* n) {
-	void* result; 
     
-	result = n -> func(n -> arg);
-    printf("Result: %d\n", *((int*) result));
+	long double sum, result;
+
+	switch (n -> arg1) {
+		case 1:
+			pthread_mutex_lock(&s1Mutex);
+			sum = s1;
+			result = n -> func(n -> arg1, n -> arg2, sum);
+			
+			if (result > EPSILON)
+				s1 = result;
+
+			pthread_mutex_unlock(&s1Mutex);
+			break;
+		case 4:
+		    pthread_mutex_lock(&s2Mutex);
+			sum = s2;
+			result = n -> func(n -> arg1, n -> arg2, sum);
+			
+			if (result > EPSILON)
+				s2 = result;
+
+			pthread_mutex_unlock(&s2Mutex);
+			break;
+	    case 5:
+			pthread_mutex_lock(&s3Mutex);
+			sum = s3;
+			result = n -> func(n -> arg1, n -> arg2, sum);
+		    
+			if (result > EPSILON)
+				s3 = result;
+
+			pthread_mutex_unlock(&s3Mutex);
+			break;
+	    default:
+		    pthread_mutex_lock(&s4Mutex);
+			sum = s4;
+			result = n -> func(n -> arg1, n -> arg2, sum);
+		    
+			if (result > EPSILON)
+				s4 = result;
+			
+			pthread_mutex_unlock(&s4Mutex);
+			break;
+	}
+
+	freeNode(n);
 }
 
 void* thPool(void* arg) {
@@ -354,13 +365,14 @@ void* thPool(void* arg) {
 			pthread_cond_wait(&qsCond, &qsMutex);
 		    
 		// Gets Work from queue
-		workNode = dequeue(work);
+		workNode = dequeue();
 
 		// Unlocks queue mutex
 		pthread_mutex_unlock(&qsMutex);
 	    
 		// Do the work
-		executeWork(workNode);
+		if (workNode)
+			executeWork(workNode);
 	}
 
 	// If thread finished execiution and there's no more work to be done
@@ -382,17 +394,19 @@ void initThreads() {
 	pthread_mutex_init(&qsMutex, NULL);
 	pthread_mutex_init(&stopMutex, NULL);
 	pthread_mutex_init(&activeMutex, NULL);
+	pthread_mutex_init(&s1Mutex, NULL);
+	pthread_mutex_init(&s2Mutex, NULL);
+	pthread_mutex_init(&s3Mutex, NULL);
+	pthread_mutex_init(&s4Mutex, NULL);
 	pthread_cond_init(&qsCond, NULL);
 	pthread_cond_init(&stopCond, NULL);
-	
-	//pthread_mutex_init(&totalOp, NULL);
 
 	for (unsigned short i = 0; i < activeThreads; i++) {
 		if (pthread_create(th + i, NULL, &thPool, NULL) != 0) {
 			unexpectedError("Error Creating Threads!");
 		}
 
-		printf("Thread %d created!\n", i);
+		//printf("Thread %d created!\n", i);
 
 		pthread_detach(th[i]);
 	}
@@ -416,44 +430,141 @@ void stopThreads() {
 		pthread_cond_wait(&stopCond, &activeMutex);
 	}
 	pthread_mutex_unlock(&activeMutex);
+
+	pthread_mutex_destroy(&qsMutex);
+	pthread_mutex_destroy(&stopMutex);
+	pthread_mutex_destroy(&activeMutex);
+	pthread_mutex_destroy(&s1Mutex);
+	pthread_mutex_destroy(&s2Mutex);
+	pthread_mutex_destroy(&s3Mutex);
+	pthread_mutex_destroy(&s4Mutex);
+	pthread_cond_destroy(&qsCond);
+	pthread_cond_destroy(&stopCond);
 }
+
+long long modPow(long long n, long long exp, long long base) {
+
+	unsigned long long result = 1;
+	unsigned long long temp = n;
+
+	temp %= base;
+	
+	while (exp) {
+
+		if (exp % 2)
+			result = (result * temp) % base;
+	
+		temp = (temp * temp) % base;
+		exp >>= 1;
+	}
+
+	return result;
+}
+
+long double lhs(int j, long long k, long double sum) {
+
+	long double r;
+
+	r = 8.0L*k +j;
+	sum = sum + modPow(16, d - k, r) / r;
+	sum = fmodl(sum, 1.0L);
+
+	//printf("(j=%d)Lhs = %Lf\n", j, sum);
+
+	return sum;
+}
+
+long double rhs(int j, long long k, long double sum) {
+	
+	long double r;
+
+	r = 8.0L*k +j;
+	sum = sum + powl(16.0L, (long double) d - k) / r;
+	sum = fmodl(sum, 1.0L);
+
+	//printf("(j=%d)Rhs = %Lf\n", j, sum);
+	
+	return sum;
+}
+
+long double bbpAlgo() { 
+
+	long double result;
+
+	initThreads();
+
+	for (long long k = 0; k < d; k++) {
+		Node* s1, *s2, *s3, *s4;
+
+		s1 = initNode(lhs, 1, k);
+		enqueue(s1);
+		
+		s2 = initNode(lhs, 4, k);
+		enqueue(s2);
+
+		s3 = initNode(lhs, 5, k);
+		enqueue(s3);
+		
+		s4 = initNode(lhs, 6, k);
+		enqueue(s4);
+	} 
+
+	for (long long k = d; k <= d + 100; k++) {
+		Node* s1, *s2, *s3, *s4;
+
+		s1 = initNode(rhs, 1, k);
+		enqueue(s1);
+		
+		s2 = initNode(rhs, 4, k);
+		enqueue(s2);
+
+		s3 = initNode(rhs, 5, k);
+		enqueue(s3);
+		
+		s4 = initNode(rhs, 6, k);
+		enqueue(s4);
+	}
+
+	stopThreads();
+
+	printf("s1: %Lf\ns2: %Lf\ns3: %Lf\ns4: %Lf\n", s1, s2, s3, s4);
+
+	result = 4.0L *s1 - 2.0L * s2 - s3 - s4;
+	result = fmodl(result, 1.0L);
+
+	return result;
+}
+
+
+void ihex (long double x) {
+	int i;
+	long double y;
+	char hx[] = "0123456789ABCDEF";
+	
+	y = x;
+
+	for (i = 0; i < PRECISION; i++){
+		y = 16. * (y - floor (y));
+		printf("%c", hx[(int) y]);
+	}
+}
+
 
 int main(int argc, char* argv[]) {
 
-	long long d;
 	long double result;
 
 	work = initQueue();
 	//results = initQueue();
 
-	checkArgs(argc, argv, &d);
-	initThreads();
+	checkArgs(argc, argv);
 
-	srand(time(NULL));
-	for (int i = 0; i < 10; i++) {
-		Node* newNode;
-		int* n = (int*) malloc(sizeof(int));
-		checkNullPointer((void*) n);
+	result = bbpAlgo(d);
+	printf("%d digits @ %lld = ", PRECISION, d);
+	ihex(result);
+	puts("");
 
-		*n = rand() % 100;
-		newNode = initNode(&sqrt2, (void*) n);
-		enqueue(work, newNode);
-		//printQueue(work);
-	}
+	free(work);
 
-	stopThreads();
-
-	/* result = bbpAlgo(d); */
-	/* printf("%d digits @ %lld = ", PRECISION, d); */
-	/* ihex(result); */
-	/* puts(""); */
-
-	/* pthread_mutex_destroy(&workQueue); */
-	/* pthread_mutex_destroy(&resQueue); */
-	/* pthread_cond_destroy(&condWork); */
-	/* pthread_cond_destroy(&condRes); */
-	/* free(work); */
-	/* free(res); */
-
-	pthread_exit(0);
+    return 0;
 }
